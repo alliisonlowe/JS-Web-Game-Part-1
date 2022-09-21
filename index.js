13458f044b07f1bd4f31c3915ee90a49830679ba
Allison Lowe <alliisonlowe@gmail.com>
2022-09-20 21:44:13 -0700

    adding more images

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ function newImage(url, left, bottom){
 
 newImage('assets/green-character.gif', 100, 150)
 newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
 /*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
