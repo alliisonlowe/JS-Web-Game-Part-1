884b634b5c3b245ccb9a9d2caacb1101a4671c47
Allison Lowe <alliisonlowe@gmail.com>
2022-09-20 21:47:24 -0700

    adding an item

--- index.js
+++ index.js
@@ -14,17 +14,13 @@ newImage('assets/pillar.png', 350, 100)
 newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
-/*let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+let sword = document.createElement('img')
+sword.src='assets/sword.png'
+sword.position='fixed'
+sword.left='500px'
+sword.bottom='405px'
+document.body.append(sword)
 
-let pineTree = document.createElement('img')
-pineTree.src = 'assets/pine-tree.png'
-pineTree.style.position = 'fixed'
-pineTree.style.left = '450px'
-pineTree.style.bottom = '200px'
-document.body.append(pineTree)*/
-
+sword.addEventListener('click', function(){
+    sword.remove()
+})
