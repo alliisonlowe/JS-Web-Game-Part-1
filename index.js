cc13e2ec29a6f02bfba3489f7879e97c5d423d16
Allison Lowe <alliisonlowe@gmail.com>
2022-09-20 21:41:15 -0700

    Creating new image function

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-let greenCharacter = document.createElement('img')
+function newImage(url, left, bottom){
+    let image = document.createElement('img')
+    image.src = url
+    image.style.position = 'fixed'
+    image.style.left = left + 'px'
+    image.style.bottom = bottom + 'px'
+    document.body.append(image)
+}
+
+newImage('assets/green-character.gif', 100, 150)
+newImage('assets/pine-tree.png', 450, 200)
+/*let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
@@ -10,5 +21,5 @@ pineTree.src = 'assets/pine-tree.png'
 pineTree.style.position = 'fixed'
 pineTree.style.left = '450px'
 pineTree.style.bottom = '200px'
-document.body.append(pineTree)
+document.body.append(pineTree)*/
 
